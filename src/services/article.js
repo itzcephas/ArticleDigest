925b5e72fa68817bacf1d36079dee45fad3acbdb
Peter <110997487+itzcephas@users.noreply.github.com>
2023-09-18 20:35:26 +0100

    first commit

---
+++ src/services/article.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+
+export const articleApi = createApi({
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'cdfdefd954mshd2f01f2211d16b4p1e982cjsn4e9cdc4999ca');
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
+        })
+    })
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
